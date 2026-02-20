af7d2fa38187e4741d54418206c10cca53900a89
sebxssj <sebassanch1223@gmail.com>
2026-02-20 16:38:02 -0600

    Sebastian Sanchez - Restaurant App

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -11,11 +11,11 @@ import { CommonModule } from '@angular/common';
 export class Menu {
 
   platillos = [
-    { nombre: 'Tacos al Pastor', precio: 40 },
+    { nombre: 'Tacos de Cabeza', precio: 40 },
     { nombre: 'Enchiladas Verdes', precio: 55 },
-    { nombre: 'Hamburguesa Especial', precio: 100 },
-    { nombre: 'Pizza Pepperoni', precio: 99 },
-    { nombre: 'quesadilla de Pollo', precio: 30 },
+    { nombre: 'Hamburguesa Loka', precio: 100 },
+    { nombre: 'Pizza Margherita', precio: 99 },
+    { nombre: 'Torta de Chorizo', precio: 30 },
     { nombre: 'Refresco', precio: 30 }
   ];
 
