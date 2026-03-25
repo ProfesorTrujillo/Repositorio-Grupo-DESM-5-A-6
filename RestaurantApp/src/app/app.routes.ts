d38e25307de47f66ef33e4b312be56da95322c7b
melisamna <melisa.mna@outlook.com>
2026-03-25 15:11:49 -0600

    Elimada carpeta RestaurantApp

--- RestaurantApp/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { PackageListComponent } from './componentes/package-list-componente/package-list-componente';
-import { PackageDetailComponente } from './componentes/package-detail-componente/package-detail-componente';
-
-export const routes: Routes = [
-  { path: '', component: PackageListComponent },
-  { path: 'detalles/:id', component: PackageDetailComponente },
-  { path: '**', redirectTo: '' }
-];
